4ae2a0d3d442796a0e0089ba35e6917e5ef3c416
Angelina Tingaeva <angelinatingaeva@yandex.ru>
2023-04-14 03:00:05 +0500

    text_x

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня НАКОНЕЦ_ТО получилось!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
